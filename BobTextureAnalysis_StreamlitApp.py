691fa0f17642ff2b5d34f0b33e9c19853539e737
Liam Ernst <wernst24@gmail.com>
2024-12-04 22:17:32 -0500

    Add test code to upload image to stapp

--- BobTextureAnalysis_StreamlitApp.py
+++ BobTextureAnalysis_StreamlitApp.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
-st.write("Test 11/4/24")
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import time
+
+from io import BytesIO
+
+import sys
+
+sys.dont_write_bytecode = True
+
+sys.tracebacklimit = 0
+
+from modules import *
+
+with st.form(key='form1', clear_on_submit=False):
+    st.markdown('test')
+
+    uploaded_file = st.file_uploader("Upload a 2D image to be analyzed. Works best with images smaller than 600x600 pixels.", type=["tif", "tiff", "png", "jpg", "jpeg"], accept_multiple_files = False, label_visibility = 'collapsed')
