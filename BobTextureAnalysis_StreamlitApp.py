296d79cb4b7fad44d93d87220386877f74fe871f
Liam Ernst <wernst24@gmail.com>
2024-12-05 10:31:26 -0500

    Add submit button to stapp

--- BobTextureAnalysis_StreamlitApp.py
+++ BobTextureAnalysis_StreamlitApp.py
@@ -20,3 +20,5 @@ with st.form(key='form1', clear_on_submit=False):
     st.markdown('test')
 
     uploaded_file = st.file_uploader("Upload a 2D image to be analyzed. Works best with images smaller than 600x600 pixels.", type=["tif", "tiff", "png", "jpg", "jpeg"], accept_multiple_files = False, label_visibility = 'collapsed')
+
+    st.form_submit_button()
