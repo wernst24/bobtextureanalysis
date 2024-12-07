c6dc67cd2da301b2dda3ccad0bb1fcf742a78039
Liam Ernst <wernst24@gmail.com>
2024-12-07 00:39:06 -0500

    Add user input for parameters

--- BobTextureAnalysis_StreamlitApp.py
+++ BobTextureAnalysis_StreamlitApp.py
@@ -24,8 +24,20 @@ with col1:
         st.markdown('test')
 
         msg = "Upload a 2D image to be analyzed. Works best with images smaller than 600x600 pixels."
+
+        # User input:
+        # Image to analyze
+        # Checkbox to invert image
+        # slider for local sigma
+
         uploaded_file = st.file_uploader(msg, type=["tif", "tiff", "png", "jpg", "jpeg"], accept_multiple_files=False, label_visibility='collapsed')
 
+        invert = st.checkbox(label="Invert image?")
+
+        inner_sigma = st.slider(min_value=1, max_value=20, step=1, label="Local sigma", help="Smaller values will emphasize higher frequecy detail, while larger values will focus on larger detail. Find what looks cool!")
+        # Adding user input to st.session_state
+
+        # Image
         if "opencv_image" not in st.session_state:
             st.session_state.opencv_image = None
         
@@ -35,22 +47,39 @@ with col1:
             opencv_image = cv.imdecode(file_bytes, 1)
             st.session_state.opencv_image = opencv_image
             st.write("Image uploaded successfully!")
+        
+
+        # Invert?
+        if "invert_image" not in st.session_state:
+            st.session_state.invert_image = None
+        
+        if invert:
+            st.session_state.invert_image = invert
 
         st.form_submit_button(label="Analyze image")
 
+
+        # Local sigma
+        if "inner_sigma" not in st.session_state:
+            st.session_state.inner_sigma = None
+        
+        if inner_sigma:
+            st.session_state.inner_sigma = inner_sigma
+
 with col2:
     if st.session_state.opencv_image is not None:
-        raw_image = color.rgb2gray(st.session_state.opencv_image)
+        raw_image_gray = color.rgb2gray(st.session_state.opencv_image)
 
+        if st.session_state.invert_image:
+            raw_image_gray = 1 - raw_image_gray
         
-        
-        coherence, two_phi = coh_ang_calc(raw_image)
+        coherence, two_phi = coh_ang_calc(raw_image_gray, sigma_inner=st.session_state.inner_sigma)
 
-        all_img = orient_hsv(raw_image, coherence, two_phi, mode='all')
-        coh_img = orient_hsv(raw_image, coherence, two_phi, mode='coherence')
-        ang_img = orient_hsv(raw_image, coherence, two_phi, mode='angle')
+        all_img = orient_hsv(raw_image_gray, coherence, two_phi, mode='all')
+        coh_img = orient_hsv(raw_image_gray, coherence, two_phi, mode='coherence')
+        ang_img = orient_hsv(raw_image_gray, coherence, two_phi, mode='angle')
 
 
-        st.image([raw_image, all_img, coh_img, ang_img])
+        st.image([raw_image_gray, all_img, coh_img, ang_img])
     else:
         st.write("No image uploaded yet")
